e9e452114ff4e50eb56bf5bc4691c8cde998f800
Andey Robins <jtuttle5@uwyo.edu>
2023-03-10 10:02:22 -0700

    Perform kata in march 2023

--- src/Yatzy.ts
+++ src/Yatzy.ts
@@ -1,197 +1,139 @@
+const count = (list: number[], val: number) => list.filter(x => x === val).length
+
 export default class Yatzy {
     private dice: number[];
   
-    constructor(d1: number, d2: number, d3: number, d4: number, _5: number) {
-      this.dice = [];
-      this.dice[0] = d1;
-      this.dice[1] = d2;
-      this.dice[2] = d3;
-      this.dice[3] = d4;
-      this.dice[4] = _5;
+    constructor(d1: number, d2: number, d3: number, d4: number, d5: number) {
+      this.dice = [d1, d2, d3, d4, d5];
     }
   
-    static chance(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var total = 0;
-      total += d1;
-      total += d2;
-      total += d3;
-      total += d4;
-      total += d5;
-      return total;
+    chance(): number {
+      // this reduce lambda sums values in this.dice
+      return this.dice.reduce((acc, val) => acc += val)
     }
   
-    static yatzy(...args: number[]): number {
-      var counts = [0, 0, 0, 0, 0, 0, 0, 0];
-      for (var i = 0; i != args.length; ++i) {
-        var die = args[i];
-        counts[die - 1]++;
+    yatzy(): number {
+      const roll = this.dice[0];
+      if (this.count(roll) === 5) {
+        return 50
       }
-      for (i = 0; i != 6; i++) if (counts[i] == 5) return 50;
-      return 0;
+      return 0
     }
   
-    static ones(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var sum = 0;
-      if (d1 == 1) sum++;
-      if (d2 == 1) sum++;
-      if (d3 == 1) sum++;
-      if (d4 == 1) sum++;
-      if (d5 == 1) sum++;
-  
-      return sum;
+    ones(): number {
+      return this.count(1) * 1
     }
   
-    static twos(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var sum = 0;
-      if (d1 == 2) sum += 2;
-      if (d2 == 2) sum += 2;
-      if (d3 == 2) sum += 2;
-      if (d4 == 2) sum += 2;
-      if (d5 == 2) sum += 2;
-      return sum;
+    twos(): number {
+      return this.count(2) * 2
     }
   
-    static threes(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var s;
-      s = 0;
-      if (d1 == 3) s += 3;
-      if (d2 == 3) s += 3;
-      if (d3 == 3) s += 3;
-      if (d4 == 3) s += 3;
-      if (d5 == 3) s += 3;
-      return s;
+    threes(): number {
+      return this.count(3) * 3
     }
-  
-    static score_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-      counts[d1 - 1]++;
-      counts[d2 - 1]++;
-      counts[d3 - 1]++;
-      counts[d4 - 1]++;
-      counts[d5 - 1]++;
-      var at;
-      for (at = 0; at != 6; at++) if (counts[6 - at - 1] >= 2) return (6 - at) * 2;
-      return 0;
-    }
-  
-    static two_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-      counts[d1 - 1]++;
-      counts[d2 - 1]++;
-      counts[d3 - 1]++;
-      counts[d4 - 1]++;
-      counts[d5 - 1]++;
-      var n = 0;
-      var score = 0;
-      for (let i = 0; i < 6; i += 1)
-        if (counts[6 - i - 1] >= 2) {
-          n++;
-          score += 6 - i;
-        }
-      if (n == 2) return score * 2;
-      else return 0;
-    }
-  
-    static four_of_a_kind(_1: number, _2: number, d3: number, d4: number, d5: number): number {
-      var tallies;
-      tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-      tallies[_1 - 1]++;
-      tallies[_2 - 1]++;
-      tallies[d3 - 1]++;
-      tallies[d4 - 1]++;
-      tallies[d5 - 1]++;
-      for (let i = 0; i < 6; i++) if (tallies[i] >= 4) return (i + 1) * 4;
-      return 0;
-    }
-  
-    static three_of_a_kind(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var t;
-      t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-      t[d1 - 1]++;
-      t[d2 - 1]++;
-      t[d3 - 1]++;
-      t[d4 - 1]++;
-      t[d5 - 1]++;
-      for (let i = 0; i < 6; i++) if (t[i] >= 3) return (i + 1) * 3;
-      return 0;
-    }
-  
-    static smallStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var tallies;
-      tallies = [0, 0, 0, 0, 0, 0, 0];
-      tallies[d1 - 1] += 1;
-      tallies[d2 - 1] += 1;
-      tallies[d3 - 1] += 1;
-      tallies[d4 - 1] += 1;
-      tallies[d5 - 1] += 1;
-      if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1) return 15;
-      return 0;
-    }
-  
-    static largeStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var tallies;
-      tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-      tallies[d1 - 1] += 1;
-      tallies[d2 - 1] += 1;
-      tallies[d3 - 1] += 1;
-      tallies[d4 - 1] += 1;
-      tallies[d5 - 1] += 1;
-      if (tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1 && tallies[5] == 1) return 20;
-      return 0;
-    }
-  
-    static fullHouse(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-      var tallies;
-      var _2 = false;
-      var i;
-      var _2_at = 0;
-      var _3 = false;
-      var _3_at = 0;
-  
-      tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-      tallies[d1 - 1] += 1;
-      tallies[d2 - 1] += 1;
-      tallies[d3 - 1] += 1;
-      tallies[d4 - 1] += 1;
-      tallies[d5 - 1] += 1;
-  
-      for (i = 0; i != 6; i += 1)
-        if (tallies[i] == 2) {
-          _2 = true;
-          _2_at = i + 1;
-        }
-  
-      for (i = 0; i != 6; i += 1)
-        if (tallies[i] == 3) {
-          _3 = true;
-          _3_at = i + 1;
-        }
-  
-      if (_2 && _3) return _2_at * 2 + _3_at * 3;
-      else return 0;
-    }
-  
+
     fours(): number {
-      var sum;
-      sum = 0;
-      for (let at = 0; at != 5; at++) {
-        if (this.dice[at] == 4) {
-          sum += 4;
-        }
-      }
-      return sum;
+      return this.count(4) * 4
     }
   
     fives(): number {
-      let s = 0;
-      var i;
-      for (i = 0; i < this.dice.length; i++) if (this.dice[i] == 5) s = s + 5;
-      return s;
+      return this.count(5) * 5
     }
   
     sixes(): number {
-      let sum = 0;
-      for (var at = 0; at < this.dice.length; at++) if (this.dice[at] == 6) sum = sum + 6;
-      return sum;
+      return this.count(6) * 6
     }
+  
+    one_pair(): number {
+      const counts = [0, 0, 0, 0, 0, 0].map((_, idx) => this.count(idx+1));
+      for (let i = 5; i >= 0; i--) {
+        if (counts[i] >= 2) {
+          return (i+1) * 2
+        }
+      }
+      return 0;
+    }
+  
+    two_pair(): number {
+      const counts = [0, 0, 0, 0, 0, 0].map((_, idx) => this.count(idx+1));
+      
+      // if we don't have the possibility of two pair, exit early
+      if (count(counts, 2) + count(counts, 3) != 2) {
+        return 0;
+      }
+
+      let score = 0;
+      for (let i = 0; i < 6; i++) {
+        if (counts[i] >= 2) {
+          score += (i+1) * 2
+        }
+      }
+      return score;
+    }
+  
+    three_of_a_kind(): number {
+      for (let i = 1; i <= 6; i++) {
+        if (this.count(i) >= 3) {
+          return i * 3
+        }
+      }
+      return 0;
+    }
+
+    four_of_a_kind(): number {
+      for (let i = 1; i <= 6; i++) {
+        if (this.count(i) >= 4) {
+          return i * 4
+        }
+      }
+      return 0;
+    }
+  
+    small_straight(): number {
+      if (
+        this.count(1) == 1 && 
+        this.count(2) == 1 && 
+        this.count(3) == 1 && 
+        this.count(4) == 1 && 
+        this.count(5) == 1) 
+      {
+        return 15;
+      }
+      return 0;
+    }
+  
+    large_straight(): number {
+      if (
+        this.count(2) == 1 && 
+        this.count(3) == 1 && 
+        this.count(4) == 1 && 
+        this.count(5) == 1 && 
+        this.count(6) == 1) 
+      {
+        return 20;
+      }
+      return 0;
+    }
+  
+    full_house(): number {
+      const counts = [0, 0, 0, 0, 0, 0].map((_, idx) => this.count(idx+1));
+      if (count(counts, 2) != 1 && count(counts, 3) != 1) {
+        return 0;
+      }
+  
+      let score = 0;
+      for (let i = 0; i < 6; i++) {
+        if (counts[i] != 0) {
+          score += counts[i] * (i+1)
+        }
+      }
+      return score;
+    }
+
+    /** count will return the number of elements with the value val
+     *  present in this.dice */
+    private count(val: number) {
+      return count(this.dice, val);
+    }
+
   }
--- test/Yatzy.test.ts
+++ test/Yatzy.test.ts
@@ -4,116 +4,158 @@ import Yatzy from '../src/Yatzy';
 
 describe('Chance', () => {
   it('scores sum of all dice', () => {
-    assert.strictEqual(15, Yatzy.chance(2, 3, 4, 5, 1));
-    assert.strictEqual(16, Yatzy.chance(3, 3, 4, 5, 1));
+    assert.strictEqual(new Yatzy(2, 3, 4, 5, 1).chance(), 15);
+    assert.strictEqual(new Yatzy(3, 3, 4, 5, 1).chance(), 16);
+    assert.strictEqual(new Yatzy(2, 6, 4, 5, 1).chance(), 18);
+    assert.strictEqual(new Yatzy(2, 3, 6, 5, 1).chance(), 17);
+    assert.strictEqual(new Yatzy(2, 3, 4, 4, 1).chance(), 14);
+    assert.strictEqual(new Yatzy(2, 3, 4, 5, 3).chance(), 17);
   });
 });
 
 describe('Yatzy', () => {
   it('scores 50', () => {
-    assert.strictEqual(50, Yatzy.yatzy(4, 4, 4, 4, 4));
-    assert.strictEqual(50, Yatzy.yatzy(6, 6, 6, 6, 6));
-    assert.strictEqual(0, Yatzy.yatzy(6, 6, 6, 6, 3));
+    assert.strictEqual(0, new Yatzy(3, 6, 6, 6, 6).yatzy());
+    assert.strictEqual(50, new Yatzy(1, 1, 1, 1, 1).yatzy());
+    assert.strictEqual(50, new Yatzy(2, 2, 2, 2, 2).yatzy());
+    assert.strictEqual(50, new Yatzy(3, 3, 3, 3, 3).yatzy());
+    assert.strictEqual(50, new Yatzy(4, 4, 4, 4, 4).yatzy());
+    assert.strictEqual(50, new Yatzy(5, 5, 5, 5, 5).yatzy());
+    assert.strictEqual(50, new Yatzy(6, 6, 6, 6, 6).yatzy());
   });
 });
 
 describe('Ones', () => {
   it('score the sum of 1s', () => {
-    assert.strictEqual(1, Yatzy.ones(1, 2, 3, 4, 5));
-    assert.strictEqual(2, Yatzy.ones(1, 2, 1, 4, 5));
-    assert.strictEqual(0, Yatzy.ones(6, 2, 2, 4, 5));
-    assert.strictEqual(4, Yatzy.ones(1, 2, 1, 1, 1));
+    assert.strictEqual(new Yatzy(6, 2, 2, 4, 5).ones(), 0);
+    assert.strictEqual(new Yatzy(1, 2, 3, 4, 5).ones(), 1);
+    assert.strictEqual(new Yatzy(1, 2, 1, 4, 5).ones(), 2);
+    assert.strictEqual(new Yatzy(2, 1, 1, 1, 1).ones(), 4);
+    assert.strictEqual(new Yatzy(1, 2, 1, 1, 1).ones(), 4);
   });
 });
 
 describe('Twos', () => {
   it('score the sum of 2s', () => {
-    assert.strictEqual(4, Yatzy.twos(1, 2, 3, 2, 6));
-    assert.strictEqual(10, Yatzy.twos(2, 2, 2, 2, 2));
+    assert.strictEqual(new Yatzy(1, 4, 3, 5, 6).twos(), 0);
+    assert.strictEqual(new Yatzy(1, 2, 3, 2, 6).twos(), 4);
+    assert.strictEqual(new Yatzy(2, 2, 2, 2, 2).twos(), 10);
+    assert.strictEqual(new Yatzy(1, 2, 2, 2, 6).twos(), 6);
+    assert.strictEqual(new Yatzy(2, 1, 6, 1, 4).twos(), 2);
+    assert.strictEqual(new Yatzy(2, 2, 3, 2, 2).twos(), 8);
   });
 });
 
 describe('Threes', () => {
   it('score the sum of 3s', () => {
-    assert.strictEqual(6, Yatzy.threes(1, 2, 3, 2, 3));
-    assert.strictEqual(12, Yatzy.threes(2, 3, 3, 3, 3));
+    assert.strictEqual(new Yatzy(1, 2, 1, 2, 4).threes(), 0);
+    assert.strictEqual(new Yatzy(1, 2, 3, 2, 3).threes(), 6);
+    assert.strictEqual(new Yatzy(2, 3, 3, 3, 3).threes(), 12);
+    assert.strictEqual(new Yatzy(3, 2, 3, 2, 3).threes(), 9);
+    assert.strictEqual(new Yatzy(1, 3, 2, 2, 1).threes(), 3);
+    assert.strictEqual(new Yatzy(1, 2, 6, 3, 6).threes(), 3);
+    assert.strictEqual(new Yatzy(3, 3, 3, 3, 3).threes(), 15);
   });
 });
 
 describe('Fours', () => {
   it('score the sum of 4s', () => {
-    assert.strictEqual(12, new Yatzy(4, 4, 4, 5, 5).fours());
-    assert.strictEqual(8, new Yatzy(4, 4, 5, 5, 5).fours());
-    assert.strictEqual(4, new Yatzy(4, 5, 5, 5, 5).fours());
+    assert.strictEqual(new Yatzy(1, 5, 5, 5, 5).fours(), 0);
+    assert.strictEqual(new Yatzy(4, 4, 4, 5, 5).fours(), 12);
+    assert.strictEqual(new Yatzy(4, 4, 5, 5, 5).fours(), 8);
+    assert.strictEqual(new Yatzy(4, 5, 5, 5, 5).fours(), 4);
+    assert.strictEqual(new Yatzy(5, 5, 5, 5, 4).fours(), 4);
+    assert.strictEqual(new Yatzy(5, 4, 4, 5, 5).fours(), 8);
+    assert.strictEqual(new Yatzy(6, 4, 4, 4, 4).fours(), 16);
   });
 });
 
 describe('Fives', () => {
-  it('score the sum of fives', () => {
-    assert.strictEqual(10, new Yatzy(4, 4, 4, 5, 5).fives());
-    assert.strictEqual(15, new Yatzy(4, 4, 5, 5, 5).fives());
-    assert.strictEqual(20, new Yatzy(4, 5, 5, 5, 5).fives());
+  it('score the sum of 5s', () => {
+    assert.strictEqual(new Yatzy(1, 1, 2, 3, 6).fives(), 0);
+    assert.strictEqual(new Yatzy(4, 4, 4, 5, 5).fives(), 10);
+    assert.strictEqual(new Yatzy(4, 4, 5, 5, 5).fives(), 15);
+    assert.strictEqual(new Yatzy(5, 5, 5, 5, 5).fives(), 25);
+    assert.strictEqual(new Yatzy(4, 1, 4, 5, 5).fives(), 10);
+    assert.strictEqual(new Yatzy(5, 5, 5, 2, 5).fives(), 20);
   });
 });
 
 describe('Sixes', () => {
-  it('score the sum of sixes', () => {
-    assert.strictEqual(0, new Yatzy(4, 4, 4, 5, 5).sixes());
-    assert.strictEqual(6, new Yatzy(4, 4, 6, 5, 5).sixes());
-    assert.strictEqual(18, new Yatzy(6, 5, 6, 6, 5).sixes());
+  it('score the sum of 6s', () => {
+    assert.strictEqual(new Yatzy(4, 4, 4, 5, 5).sixes(), 0);
+    assert.strictEqual(new Yatzy(4, 4, 6, 5, 5).sixes(), 6);
+    assert.strictEqual(new Yatzy(6, 5, 6, 6, 5).sixes(), 18);
+    assert.strictEqual(new Yatzy(4, 5, 6, 6, 5).sixes(), 12);
+    assert.strictEqual(new Yatzy(6, 6, 6, 6, 5).sixes(), 24);
+    assert.strictEqual(new Yatzy(6, 5, 6, 6, 6).sixes(), 24);
   });
 });
 
 describe('One pair', () => {
   it('scores the sum of the highest pair', () => {
-    assert.strictEqual(6, Yatzy.score_pair(3, 4, 3, 5, 6));
-    assert.strictEqual(10, Yatzy.score_pair(5, 3, 3, 3, 5));
-    assert.strictEqual(12, Yatzy.score_pair(5, 3, 6, 6, 5));
+    assert.strictEqual(new Yatzy(1, 2, 6, 3, 4).one_pair(), 0);
+    assert.strictEqual(new Yatzy(1, 2, 2, 3, 4).one_pair(), 4);
+    assert.strictEqual(new Yatzy(3, 4, 3, 5, 6).one_pair(), 6);
+    assert.strictEqual(new Yatzy(5, 3, 3, 3, 5).one_pair(), 10);
+    assert.strictEqual(new Yatzy(5, 3, 6, 6, 5).one_pair(), 12);
+    assert.strictEqual(new Yatzy(4, 4, 6, 1, 2).one_pair(), 8);
   });
 });
 
 describe('Two pair', () => {
   it('scores the sum of the two pairs', () => {
-    assert.strictEqual(16, Yatzy.two_pair(3, 3, 5, 4, 5));
-    assert.strictEqual(16, Yatzy.two_pair(3, 3, 5, 5, 5));
+    // refactor using the constructor
+    assert.strictEqual(new Yatzy(3, 3, 5, 4, 5).two_pair(), 16);
+    assert.strictEqual(new Yatzy(3, 3, 5, 5, 5).two_pair(), 16);
+    // not enough coverage
   });
 });
 
 describe('Three of a kind', () => {
   it('scores the sum of the three of the kind', () => {
-    assert.strictEqual(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5));
-    assert.strictEqual(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5));
-    assert.strictEqual(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5));
+    assert.strictEqual(new Yatzy(3, 3, 3, 4, 5).three_of_a_kind(), 9);
+    assert.strictEqual(new Yatzy(5, 3, 5, 4, 5).three_of_a_kind(), 15);
+    assert.strictEqual(new Yatzy(3, 3, 3, 3, 5).three_of_a_kind(), 9);
+    assert.strictEqual(new Yatzy(1, 2, 3, 4, 5).three_of_a_kind(), 0);
+    assert.strictEqual(new Yatzy(1, 2, 2, 2, 3).three_of_a_kind(), 6);
+    assert.strictEqual(new Yatzy(1, 2, 4, 4, 4).three_of_a_kind(), 12);
+    assert.strictEqual(new Yatzy(2, 6, 6, 3, 6).three_of_a_kind(), 18);
   });
 });
 
 describe('Four of a kind', () => {
   it('scores the sum of the four of the kind', () => {
-    assert.strictEqual(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5));
-    assert.strictEqual(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5));
-    assert.strictEqual(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 3));
+    assert.strictEqual(new Yatzy(1, 2, 3, 4, 5).four_of_a_kind(), 0);
+    assert.strictEqual(new Yatzy(3, 3, 3, 1, 5).four_of_a_kind(), 0);
+    assert.strictEqual(new Yatzy(3, 3, 3, 3, 5).four_of_a_kind(), 12);
+    assert.strictEqual(new Yatzy(5, 5, 5, 4, 5).four_of_a_kind(), 20);
+    assert.strictEqual(new Yatzy(3, 3, 3, 3, 3).four_of_a_kind(), 12);
+    assert.strictEqual(new Yatzy(6, 3, 6, 6, 6).four_of_a_kind(), 24);
+    assert.strictEqual(new Yatzy(3, 2, 2, 2, 2).four_of_a_kind(), 8);
+
   });
 });
 
 describe('Small straight', () => {
-  it('scores 15', () => {
-    assert.strictEqual(15, Yatzy.smallStraight(1, 2, 3, 4, 5));
-    assert.strictEqual(15, Yatzy.smallStraight(2, 3, 4, 5, 1));
-    assert.strictEqual(0, Yatzy.smallStraight(1, 2, 2, 4, 5));
+  it('scores 15 for a small straight', () => {
+    assert.strictEqual(new Yatzy(1, 2, 3, 4, 5).small_straight(), 15);
+    assert.strictEqual(new Yatzy(2, 3, 4, 5, 1).small_straight(), 15);
+    assert.strictEqual(new Yatzy(1, 2, 2, 4, 5).small_straight(), 0);
   });
 });
 
 describe('Large straight', () => {
-  it('scores 20', () => {
-    assert.strictEqual(20, Yatzy.largeStraight(6, 2, 3, 4, 5));
-    assert.strictEqual(20, Yatzy.largeStraight(2, 3, 4, 5, 6));
-    assert.strictEqual(0, Yatzy.largeStraight(1, 2, 2, 4, 5));
+  it('scores 20 for a large straight', () => {
+    assert.strictEqual(new Yatzy(6, 2, 3, 4, 5).large_straight(), 20);
+    assert.strictEqual(new Yatzy(2, 3, 4, 5, 6).large_straight(), 20);
+    assert.strictEqual(new Yatzy(1, 2, 2, 4, 5).large_straight(), 0);
   });
 });
 
 describe('Full house', () => {
   it('scores the sum of the full house', () => {
-    assert.strictEqual(18, Yatzy.fullHouse(6, 2, 2, 2, 6));
-    assert.strictEqual(0, Yatzy.fullHouse(2, 3, 4, 5, 6));
+    assert.strictEqual(new Yatzy(6, 2, 2, 2, 6).full_house(), 18);
+    assert.strictEqual(new Yatzy(2, 3, 4, 5, 6).full_house(), 0);
   });
 });
